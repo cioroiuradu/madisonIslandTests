9933f9f66a400b623e739762d6c377bd32d65179
cioroiuradu <cioroiu.radu@gmail.com>
2019-02-20 21:20:41 +0200

    studied page object model

--- src/test/java/org/fasttrackit/ShoppingCartTest.java
+++ src/test/java/org/fasttrackit/ShoppingCartTest.java
@@ -1,10 +1,13 @@
 package org.fasttrackit;
 
+import org.fasttrackit.pageobjects.Header;
+import org.fasttrackit.pageobjects.ProductsGrid;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,25 +16,25 @@ public class ShoppingCartTest {
 
     @Test
     public void addToCartFromSearchResultsTest() {
-        System.setProperty("webdriver.chrome.driver","D:\\madisonIslandTests\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", AppConfig.getChromeDriverPath());
         WebDriver driver = new ChromeDriver();
         driver.get("https://fasttrackit.org/selenium-test/");
 
         String keyWord = "vase";
-        driver.findElement(By.id("search")).
-                sendKeys(keyWord);
+        Header header = PageFactory.initElements(driver, Header.class);
+        header.search(keyWord);
 
-        driver.findElement(By.tagName("button")).click();
+        ProductsGrid productsGrid = PageFactory.initElements(driver, ProductsGrid.class);
 
-        String productToCart ="Herald Glass Vase";
-        driver.findElement(By.xpath(
-                "//div[@class='product-info' and .//a[text()='"+productToCart+"']]//button[@title='Add to Cart']")).click();
+        String productName = "Herald Glass Vase";
+        productsGrid.addProductToCart(productName, driver);
+
 
         String cart = driver.findElement(By.className("success-msg")).getText();
 
-        assertThat("Unexpected success message", cart, is(productToCart + " was added to your shopping cart."));
+        assertThat("Unexpected success message", cart, is(productName + " was added to your shopping cart."));
 
-        WebElement productNameInCart = driver.findElement(By.xpath("//table[@id='shopping-cart-table']//a[text()='" + productToCart + "']"));
+        WebElement productNameInCart = driver.findElement(By.xpath("//table[@id='shopping-cart-table']//a[text()='" + productName + "']"));
 
         assertThat("Product not displayed in cart.", productNameInCart.isDisplayed());
 
